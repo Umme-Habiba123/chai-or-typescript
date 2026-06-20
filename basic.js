56cacc87cacd10c5de4b8c29bab114935aab3d0b
Umme-Habiba123 <habibaislammim@gmail.com>
2026-06-20 22:06:06 +0600

    chai or typescript

--- basic.js
+++ basic.js
@@ -2,4 +2,4 @@ function greet(name) {
   return `Hello ${name}`;
 }
 
-console.log(greet("MahiyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaa") )
+console.log(greet("MahiyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") )
