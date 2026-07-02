099d92d607d5e86c4b857073de119088622e4448
Umme-Habiba123 <habibaislammim@gmail.com>
2026-07-02 20:20:25 +0600

    added new one

--- basic.js
+++ basic.js
@@ -2,4 +2,4 @@ function greet(name) {
   return `Hello ${name}`;
 }
 
-console.log(greet("Mahiyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") )
+console.log(greet("Mahiyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") )
